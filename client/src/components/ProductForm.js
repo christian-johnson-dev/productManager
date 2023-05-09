c82966381b10b7a835c2f5089dcf7b1c1e5966df
Christian Johnson <charcoaldust@mac.com>
2023-05-08 19:59:39 -0700

    Initial commit

---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+const ProductForm = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+  const handleDescChange = (event) => {
+    setDescription(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //don't forget your synthetic submit function
+    axios
+      .post("http://localhost:8000/api/product", {
+        title,
+        price,
+        description,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setTitle("");
+        setPrice("");
+        setDescription("");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="">Title</label>
+        <br />
+        <input type="text" value={title} onChange={handleTitleChange} />
+      </p>
+      <p>
+        <label htmlFor="">Price</label>
+        <br />
+        <input type="number" value={price} onChange={handlePriceChange} />
+      </p>
+      <p>
+        <label htmlFor="">Description</label>
+        <br />
+        <input
+          type="textarea"
+          value={description}
+          onChange={handleDescChange}
+        />
+      </p>
+      <button type="submit">Create</button>
+    </form>
+  );
+};
+export default ProductForm;
